ffbe870c7241036a6f2479d7ca7f0fb1803d105f
penta0410 <144640030+penta0410@users.noreply.github.com>
2024-04-02 15:10:24 +0900

    a

---
+++ Src/Transparent/Transparent.cpp
@@ -0,0 +1,37 @@
+#include "DxLib.h"
+#include "Transparent.h"
+
+//‰Šú‰»
+int Flag = true;
+
+//“§‰ßŠÖ”
+int Transparent(int alpha)
+{
+	//“§‰ßˆ—
+	//BottanAlphaFlag‚ªtrue‚Ì‚É“§‰ß‚·‚é
+	if (Flag)
+	{
+		alpha -= ALPHA_SPEED;
+		
+		//BottanAlpha‚ª0ˆÈ‰º‚ÌBottanAlphaFlag‚ğfalse‚É‚·‚é
+		if (alpha <= 0)
+		{
+			Flag = false;
+		}
+		
+	}
+	//BottanAlphaFlag‚ªfalse‚Ì‚É’Êí‚É–ß‚·
+	if (!Flag)
+	{
+		alpha += ALPHA_SPEED;
+
+		//BottanAlpha‚ª100ˆÈã‚ÌBottanAlphaFlag‚ğtrue‚É‚·‚é
+		if (alpha >= 100)
+		{
+			Flag = true;
+		}
+		
+	}
+	return alpha;
+	
+}
