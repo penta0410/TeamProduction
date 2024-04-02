c931ac1d75894a3cf563d93c1fe5a9c2479bb01e
gurabur <165613417+gurabur@users.noreply.github.com>
2024-04-02 20:27:30 +0900

    落合敵少し

--- Src/Enemy/enemy.h
+++ Src/Enemy/enemy.h
@@ -1,1 +1,27 @@
 #pragma once
+
+//敵情報
+#define ENEMY_PATH		//敵のパス
+#define ENEMY_MAX_NUM	//敵の数
+
+//敵情報
+struct EnemyInfo
+{
+	int handle;		//画像ハンドル
+	int x, y;		//座標
+	int enemyHP;	//敵のライフ			
+	bool isActive;	//敵の生存フラグ
+};
+
+class Enemy
+{
+public:
+
+	//敵初期化
+
+	//敵の描画
+
+	//敵全滅チェック
+
+};
+
