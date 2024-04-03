f206af8b6f4928b044cf6ee34cd58b510717333f
gurabur <165613417+gurabur@users.noreply.github.com>
2024-04-03 14:35:18 +0900

    更新4/3

---
+++ Src/Enemy/enemy.cpp
@@ -0,0 +1,30 @@
+#include "DxLib.h"
+#include "enemy.h"
+#include "../Scene/Scene.h"
+#include "../Collision/Collision.h"
+#include "../Player/player.h"
+
+Enemy::Enemy()
+{
+
+}
+
+Enemy::~Enemy()
+{
+
+}
+
+void Enemy::InitEnemy()
+{
+
+}
+
+void Enemy::DrawEnemy()
+{
+
+}
+
+bool Enemy::IsAllKillEnemy()
+{
+
+}
